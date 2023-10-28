3a737ff8f459c95147f21f672840e5348a8dd7f9
Lautaro P <112022570+blue-h4ck@users.noreply.github.com>
2023-10-28 18:18:21 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
+/*
+The 3n+1 problem, also known as the Collatz conjecture, is a simple mathematical problem that involves following a sequence of numbers. The problem is stated as follows:
+
+Start with any positive integer n.
+If n is even, divide it by 2.
+If n is odd, multiply it by 3 and add 1.
+Repeat the process with the new value obtained.
+Continue this sequence until n becomes equal to 1.
+The conjecture suggests that, regardless of the starting value of n, this sequence will always eventually reach the number 1. However, this statement has not been proven mathematically and remains an unsolved problem in mathematics. 
+The 3n+1 problem is often used as a simple example in computer science and mathematics to illustrate concepts related to sequences, loops, and computational complexity.
+*/
+
 package main
 
 import (
